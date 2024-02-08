86c83570a2cfc24a655e262f2672eb218fdd0ded
Yoshi <andreolienzo@gmail.com>
2024-02-08 14:48:56 +0100

    Le post fonctionne mon bibou

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
-from typing import Annotated
-
 from fastapi import FastAPI, UploadFile, File
+import os
 
 app = FastAPI()
+os.makedirs("uploads", exist_ok=True)
 
 @app.post("/")
-async def post_file(file: Annotated[bytes, File()]):
-    content = file.decode("utf-8")
-    return {"file_content": content}
+async def post_file(file: UploadFile = File(...)):
+    content = await file.read()
+    with open(f"uploads/{file.filename}", "wb") as file_object:
+        file_object.write(content)
 
 @app.get("/{fileId}")
 async def get_file(name: str):
