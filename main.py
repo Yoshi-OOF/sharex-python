9a5e18c3cf5cbbe14456fe3a2e4868729a1fb26a
v0ltis <barnabe.havard@gmail.com>
2024-02-08 18:49:13 +0100

    Added settings support + now returning the URL of the posted file

--- main.py
+++ main.py
@@ -3,6 +3,23 @@ from fastapi.security import APIKeyHeader
 import os
 import random
 import string
+import yaml
+
+
+# Load the configuration file
+config = yaml.load(open("config.yml"), Loader=yaml.FullLoader)
+
+
+# Add a trailing slash if it's not present
+url = config["url"] + "" if config["url"][-1] == "/" else "/"
+
+
+# Create the upload directory if it doesn't exist
+upload_dir = config["upload_directory"]
+os.makedirs(upload_dir, exist_ok=True)
+
+
+# Create a FastAPI instance
 app = FastAPI()
 os.makedirs("uploads", exist_ok=True)
 
@@ -15,24 +32,35 @@ async def check_api_key(api_key: str = Depends(api_key_header)):
         raise HTTPException(status_code=401, detail="Unauthorized")
     return api_key
 
+
 @app.post("/")
 async def post_file(file: UploadFile = File(...), api_key: str = Depends(check_api_key)):
     content = await file.read()
+
+    # If the file size is greater than the maximum file size
+    if config["max_file_size"] > -1 and len(content) > 1000000 and config["max_file_size"]:
+        raise HTTPException(status_code=413, detail="File too large >:(")
+
+    # Retrieve file extension
     extension = os.path.splitext(file.filename)[1]
     file_name = ''.join(random.choices(string.ascii_letters + string.digits, k=32))
     file_path = f"uploads/{file_name}{extension}"
+
+    # Generate a random file name (32 characters)
+    file_name = ''.join(random.choices(string.ascii_letters + string.digits, k=config["filename_size"]))
+
+    # Choosing where to save the file
+    file_path = f"{upload_dir}/{file_name}{extension}"
+
+    # Write the file to the specified location
     with open(file_path, "wb") as file_object:
         file_object.write(content)
-    return {
-        "file_name": file_name,
-        "extension": extension,
-        "file_path": file_path
-    }
 
-@app.get("/{fileId}")
-async def get_file(name: str):
-    return {"message": f"Hello {name}"}
+    # Return the URL of the uploaded file
+    return f'{url}{file_name}{extension}'
 
+
+    file_path = f"{upload_dir}/{file_id}"
 if __name__ == "__main__":
     import uvicorn
     print("Running the ShareX server.")
