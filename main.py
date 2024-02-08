308c97ae0bfbf61d18fcec2a8ae62e08e1c07956
Yoshi <andreolienzo@gmail.com>
2024-02-08 13:27:26 +0100

    uwu

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
-from fastapi import FastAPI
+from typing import Annotated
+
+from fastapi import FastAPI, UploadFile, File
 
 app = FastAPI()
 
-
 @app.post("/")
-async def post_file():
-    return {"message": "Hello World"}
-
+async def post_file(file: Annotated[bytes, File()]):
+    content = file.decode("utf-8")
+    return {"file_content": content}
 
 @app.get("/{fileId}")
 async def get_file(name: str):
