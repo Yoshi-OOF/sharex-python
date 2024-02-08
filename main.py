d868c7c490c1e29811cd66c88a25971556efa76e
v0ltis <barnabe.havard@gmail.com>
2024-02-08 21:24:25 +0100

    Final version perhaps ?

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
-from fastapi import FastAPI, UploadFile, File, Depends, HTTPException
+from fastapi import FastAPI, UploadFile, File, Depends, HTTPException, Request
 from fastapi.security import APIKeyHeader
 from fastapi.responses import PlainTextResponse, FileResponse
+
+from streaming_form_data import StreamingFormDataParser
+from streaming_form_data.targets import FileTarget, ValueTarget
+from streaming_form_data.validators import MaxSizeValidator
+import streaming_form_data
+
+from starlette.requests import ClientDisconnect
 
 import os
 import random
 import string
 import yaml
+
+from classes import *
 
 
 # Load the configuration file
@@ -40,30 +49,53 @@ async def check_api_key(api_key: str = Depends(api_key_header)):
         raise HTTPException(status_code=403, detail="Invalid secret Key >:(")
     return api_key
 
+async def process_file(request: Request):
+    body_validator = MaxBodySizeValidator(config["max_file_size"])
+    filename = request.headers.get('Filename')
+
+    if not filename:
+        raise HTTPException(status_code=422,
+                            detail='Filename header is missing')
+    try:
+        file_extension = os.path.splitext(filename)[1]
+        file_name = ''.join(random.choices(string.ascii_letters + string.digits, k=config["filename_size"]))
+        filepath = f"{upload_dir}/{file_name}{file_extension}"
+        file_ = FileTarget(filepath, validator=MaxSizeValidator(config["max_file_size"]))
+        data = ValueTarget()
+        parser = StreamingFormDataParser(headers=request.headers)
+        parser.register('file', file_)
+        parser.register('data', data)
+
+        async for chunk in request.stream():
+            body_validator(chunk)
+            parser.data_received(chunk)
+
+        return file_name + file_extension
+
+    except ClientDisconnect:
+        print("Client Disconnected")
+    except MaxBodySizeException as e:
+        raise HTTPException(status_code=413,
+                            detail=f'Maximum request body size limit ({MAX_REQUEST_BODY_SIZE} bytes) exceeded ({e.body_len} bytes read)')
+    except streaming_form_data.validators.ValidationError:
+        raise HTTPException(status_code=413,
+                            detail=f'Maximum file size limit ({MAX_FILE_SIZE} bytes) exceeded')
+    except Exception:
+        raise HTTPException(status_code=500,
+                            detail='There was an error uploading the file')
+
+    if not file_.multipart_filename:
+        raise HTTPException(status_code=422, detail='File is missing')
+
+
 
 @app.post("/", response_class=PlainTextResponse)
-async def post_file(file: UploadFile = File(...), api_key: str = Depends(check_api_key)):
-    content = await file.read()
+async def post_file(request: Request, api_key: str = Depends(check_api_key)):
+    file_name = await process_file(request)
 
-    # If the file size is greater than the maximum file size
-    if -1 < config["max_file_size"] < len(content):
-        raise HTTPException(status_code=413, detail="File too large >:(")
-
-    # Retrieve file extension
-    extension = os.path.splitext(file.filename)[1]
-
-    # Generate a random file name (32 characters)
-    file_name = ''.join(random.choices(string.ascii_letters + string.digits, k=config["filename_size"]))
-
-    # Choosing where to save the file
-    file_path = f"{upload_dir}/{file_name}{extension}"
-
-    # Write the file to the specified location
-    with open(file_path, "wb") as file_object:
-        file_object.write(content)
 
     # Return the URL of the uploaded file
-    return f"{url}{file_name}{extension}"
+    return f"{url}{file_name}"
 
 
 @app.get("/{file_id}")
@@ -85,5 +117,5 @@ if __name__ == "__main__":
     uvicorn.run(app,
                 host=config["internal_host"],
                 port=config["internal_port"],
-                log_level=config["log_level"]
+                log_level=config["log_level"],
                 )
