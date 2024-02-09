cf8fd6523d15a6e63820f52d2212b6a899ad5837
v0ltis <voltis.discord@gmail.com>
2024-02-09 12:23:52 +0100

    Optimized imports, now using tools.py, files are now deleted when an error occurs.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, UploadFile, File, Depends, HTTPException, Request
+from fastapi import FastAPI, Depends, HTTPException, Request
 from fastapi.security import APIKeyHeader
 from fastapi.responses import PlainTextResponse, FileResponse
 
@@ -10,12 +10,10 @@ import streaming_form_data
 from starlette.requests import ClientDisconnect
 
 import os
-import random
-import string
 import yaml
 
 from classes import *
-
+from tools import name_gen, cancel_upload
 
 # Load the configuration file
 config = yaml.load(open("config.yml"), Loader=yaml.FullLoader)
@@ -35,7 +33,6 @@ os.makedirs(upload_dir, exist_ok=True)
 app = FastAPI()
 api_key_header = APIKeyHeader(name="secret-key", auto_error=False)
 
-
 async def check_api_key(api_key: str = Depends(api_key_header)):
 
     # If the secret key is not provided
@@ -49,50 +46,51 @@ async def check_api_key(api_key: str = Depends(api_key_header)):
         raise HTTPException(status_code=403, detail="Invalid secret Key >:(")
     return api_key
 
+
 async def process_file(request: Request):
     body_validator = MaxBodySizeValidator(config["max_file_size"])
     filename = request.headers.get('Filename')
 
-    if not filename:
-        raise HTTPException(status_code=422,
-                            detail='Filename header is missing')
+    # Generate a random name for the file
+    file_path, file_name = name_gen(filename, upload_dir, config["filename_size"])
+
     try:
-        file_extension = os.path.splitext(filename)[1]
-        file_name = ''.join(random.choices(string.ascii_letters + string.digits, k=config["filename_size"]))
-        filepath = f"{upload_dir}/{file_name}{file_extension}"
-        file_ = FileTarget(filepath, validator=MaxSizeValidator(config["max_file_size"]))
+        # Create the file target
+        file_ = FileTarget(file_path, validator=MaxSizeValidator(config["max_file_size"]))
         data = ValueTarget()
         parser = StreamingFormDataParser(headers=request.headers)
         parser.register('file', file_)
         parser.register('data', data)
 
+        # Process the request and start writing the file
         async for chunk in request.stream():
             body_validator(chunk)
             parser.data_received(chunk)
 
-        return file_name + file_extension
+        # Return the file name if the file was uploaded successfully
+        return file_name
 
     except ClientDisconnect:
-        print("Client Disconnected")
+        cancel_upload(file_path)
+
     except MaxBodySizeException as e:
+        cancel_upload(file_path)
         raise HTTPException(status_code=413,
                             detail=f'Maximum request body size limit ({config["max_file_size"]} bytes) exceeded ({e.body_len} bytes read)')
     except streaming_form_data.validators.ValidationError:
+        cancel_upload(file_path)
         raise HTTPException(status_code=413,
                             detail=f'Maximum file size limit ({config["max_file_size"]} bytes) exceeded')
     except Exception:
+        cancel_upload(file_path)
         raise HTTPException(status_code=500,
                             detail='There was an error uploading the file')
-
-    if not file_.multipart_filename:
-        raise HTTPException(status_code=422, detail='File is missing')
-
 
 
 @app.post("/", response_class=PlainTextResponse)
 async def post_file(request: Request, api_key: str = Depends(check_api_key)):
-    file_name = await process_file(request)
 
+    file_name = await process_file(request)
 
     # Return the URL of the uploaded file
     return f"{url}{file_name}"
